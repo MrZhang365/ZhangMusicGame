56036bbc160a40bc9dc634f9332be1aa4e7e018f
MrZhang365 <Xiao_Zhang_123@outlook.com>
2024-02-12 22:11:03 +0800

    修复浏览器禁止自动播放导致的BUG

--- media.js
+++ media.js
@@ -5,20 +5,20 @@ function onBeat() {
     aBlock('#44FF00', random(0, 6))
 }
 
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-const sourceNode = audioCtx.createMediaElementSource($('#bgm'));
-sourceNode.connect(audioCtx.destination);
-
-const analyser = audioCtx.createAnalyser();
-analyser.fftSize = 2048; // FFT大小设定为2048
-sourceNode.connect(analyser);
-analyser.connect(audioCtx.destination);
- 
-// 将音频数据转换成Float32Array格式
-const bufferLength = analyser.frequencyBinCount;
-const dataArray = new Float32Array(bufferLength);
- 
 function getBeats() {
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    const sourceNode = audioCtx.createMediaElementSource($('#bgm'));
+    sourceNode.connect(audioCtx.destination);
+
+    const analyser = audioCtx.createAnalyser();
+    analyser.fftSize = 2048; // FFT大小设定为2048
+    sourceNode.connect(analyser);
+    analyser.connect(audioCtx.destination);
+    
+    // 将音频数据转换成Float32Array格式
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Float32Array(bufferLength);
+
     function calculateBeatStrength() {
         analyser.getFloatTimeDomainData(dataArray);
         
